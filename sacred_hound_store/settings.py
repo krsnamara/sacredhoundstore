e46b29923891381bcfde233c853338e0ee6e3db7
krsnamara <martinj.fitzpatrick@gmail.com>
2023-08-13 11:54:38 -0400

    removing thr csrf trailing slash

--- sacred_hound_store/settings.py
+++ sacred_hound_store/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://sacredhoundstore-production.up.railway.app/",
+    "https://sacredhoundstore-production.up.railway.app",
 ]
 
 # Application definition
